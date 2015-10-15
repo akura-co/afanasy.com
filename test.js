a3453a0d143826f15e7cda88d6f8434db1c06883
Raabb Ajam <raabbajam@gmail.com>
2015-10-16 00:59:33 +0700

    Added express app

---
+++ test.js
@@ -0,0 +1,24 @@
+var
+  app = require('./app'),
+  express = require('express')(),
+  request = require('supertest'),
+  server
+
+  express.use('/afanasy.com', app)
+
+describe('test mount', function () {
+  beforeEach(function () {
+    server = express.listen(express.get('port') || 3000)
+  })
+  afterEach(function (done) {
+    server.close(done)
+  })
+  it('mounted from other express app', function (done) {
+    request(server).
+      get('/afanasy.com/').
+      expect(function (res) {
+        console.log(res.text);
+      }).
+      expect(200, done)
+  })
+})
